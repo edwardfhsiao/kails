bb3dcfdbb5351379b7b9d77659a281acc37d102e
edwardfhsiao <edwardfhsiao@gmail.com>
2016-09-22 10:31:36 +0800

    optimize

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -6,23 +6,39 @@ const postcssImport = require('postcss-import');
 const cssnext = require('postcss-cssnext');
 const postcssReporter = require("postcss-reporter");
 
-const config = _.merge({}, base);
+const config = _.merge(base, {
+  devtool: 'cheap-module-source-map'
+});
 
 config.output = _.merge(config.output, {
   filename: '[name]_bundle-[chunkhash].js'
 });
 
 config.plugins.push(
-  new webpack.optimize.UglifyJsPlugin(),
-  new webpack.optimize.CommonsChunkPlugin('application', 'application_bundle-[hash].js'),
-  new webpack.optimize.OccurenceOrderPlugin(),
-  new ExtractTextPlugin('[name]_bundle-[hash].css', {
-    allChunks: true
-  }),
   new webpack.DefinePlugin({
     'process.env': {
        NODE_ENV: JSON.stringify('production')
      }
+  }),
+  new webpack.optimize.CommonsChunkPlugin('application', 'application_bundle-[hash].js'),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: false
+    }
+  }),
+  new webpack.optimize.MinChunkSizePlugin({
+    compress: {
+      warnings: false
+    }
+  }),
+  // new webpack.optimize.DedupePlugin(),
+  new webpack.optimize.OccurenceOrderPlugin(),
+  new webpack.optimize.AggressiveMergingPlugin({
+      minSizeReduce: 1.5,
+      moveToParents: true
+  }),
+  new ExtractTextPlugin('[name]_bundle-[hash].css', {
+    allChunks: true
   })
 );
 
